393b2d1e3353d0574103551737c84b01213714cb
Amari <amari@amari.wtf>
2024-11-06 17:41:08 +0000

    Initial Commit

---
+++ src/main/kotlin/wtf/amari/hub/Hub.kt
@@ -0,0 +1,14 @@
+package wtf.amari.hub
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Hub : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
