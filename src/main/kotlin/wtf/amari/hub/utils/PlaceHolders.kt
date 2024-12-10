91cc66fcdbe13a7247a66eec47104cb3a580e676
Amari <amari@amari.wtf>
2024-12-10 19:57:33 +0000

    Placeholder remove comments and refactor

--- src/main/kotlin/wtf/amari/hub/utils/PlaceHolders.kt
+++ src/main/kotlin/wtf/amari/hub/utils/PlaceHolders.kt
@@ -5,20 +5,16 @@ import org.bukkit.entity.Player
 
 class PlaceHolders : PlaceholderExpansion() {
 
-    override fun getIdentifier() = "prison"
+    override fun getIdentifier() = "hub"
+
     override fun getAuthor() = "Amari"
+
     override fun getVersion() = "1.0"
 
-    /**
-     * Handles placeholder requests for player-specific data.
-     * @param player The player for whom the placeholder is requested.
-     * @param identifier The placeholder identifier.
-     * @return The placeholder value as a string.
-     */
     override fun onPlaceholderRequest(player: Player?, identifier: String): String {
         if (player == null) return ""
         return when (identifier) {
-            "player" -> (player.name)
+            "player" -> player.name
             else -> ""
         }
     }
