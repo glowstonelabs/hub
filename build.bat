df218eafa143f1da9add73b997d18efc0b5181c3
Amari <amari@amari.wtf>
2024-12-10 22:06:37 +0000

    delete the new configs to get them updated

--- build.bat
+++ build.bat
@@ -7,6 +7,7 @@ set target=C:\Users\Amari\Desktop\TESTNET\hub\plugins
 
 call .\gradlew shadowJar
 copy /y ".\build\libs\%plugin%-%version%-all.jar" "%target%\%plugin%-%version%.jar"
-del /f "%target%\%plugin%\config.yml"
-
+del /f "%target%\%plugin%\settings.yml"
+del /f "%target%\%plugin%\lang.yml"
+del /f "%target%\%plugin%\serverSelector.yml"
 ENDLOCAL
