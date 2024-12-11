0ffc511ea319ef76b6c7af7d9e170954b511dfdd
Amari <amari@amari.wtf>
2024-12-11 15:29:03 +0000

    Made it so console can clear the chat

--- src/main/kotlin/wtf/amari/hub/commands/staff/ClearChat.kt
+++ src/main/kotlin/wtf/amari/hub/commands/staff/ClearChat.kt
@@ -11,21 +11,16 @@ package wtf.amari.hub.commands.staff
 
 import me.honkling.commando.common.annotations.Command
 import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import wtf.amari.hub.utils.mm
 
 /**
  * Clears the chat for all players without the clear chat permission and sends a confirmation message.
  *
- * @param executor The player executing the command.
+ * @param sender The command sender (player or console).
  */
-fun clearChat(executor: Player) {
-    // Validate user input
-    if (executor !is Player) {
-        executor.sendMessage("&cOnly players can execute this command.".mm())
-        return
-    }
-
+fun clearChat(sender: CommandSender) {
     // Clear the chat by broadcasting 100 empty lines to players without the permission
     val playersToClear = Bukkit.getOnlinePlayers().filter { !it.hasPermission("hub.staff.clearchat") }
     repeat(100) {
@@ -33,5 +28,6 @@ fun clearChat(executor: Player) {
     }
 
     // Send a confirmation message to all players
-    Bukkit.broadcast("&cChat has been cleared by &e${executor.name}".mm())
+    val senderName = if (sender is Player) sender.name else "Console"
+    Bukkit.broadcast("&cChat has been cleared by &e$senderName".mm())
 }
